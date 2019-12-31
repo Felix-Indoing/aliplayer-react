171fc3641d4f6d53a3407516df7d84af5f000676
Felix <lcz.link@outlook.com>
2019-12-31 20:19:31 +0800

    Memorized version fetch-js-from-cdn.ts and bump to 2.8.7

--- src/fetch-js-from-cdn.ts
+++ src/fetch-js-from-cdn.ts
@@ -1,19 +1,42 @@
 declare global {
-    interface Window { [key: string]: any; }
+    interface Window {[key: string]: any;}
 }
 
-export default function fetchJsFromCDN(src: string, externals: string[] = []): Promise<any[]> {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script')
-        script.setAttribute('src', src)
-        script.addEventListener('load', () => {
-            resolve(externals.map(key => {
-                const ext = window[key];
-                typeof ext === 'undefined' && console.warn(`No external named '${key}' in window`)
-                return ext
-            }))
-        })
-        script.addEventListener('error', reject)
-        document.body.appendChild(script)
-    });
+interface CacheItem {
+    element: HTMLElement
+    promise: Promise<void>
+}
+
+function addScriptTag(src: string): CacheItem {
+    const element = document.createElement('script')
+    element.setAttribute('src', src)
+
+    const promise = new Promise<void>((resolve, reject) => {
+        element.addEventListener('error', reject)
+        element.addEventListener('load', () => resolve())
+        document.body.appendChild(element)
+    })
+
+    return { element, promise }
+}
+
+function getExpectFromWindow(expects: string[]): any[] {
+    return expects.map((key: string) => {
+        const ext: any = window[key]
+        typeof ext === undefined && console.error(`No external named '${key}' in window`)
+        return ext
+    })
+}
+
+const CachedMap = new Map<string, CacheItem>()
+
+export default async function fetchJsFromCDN(src: string, expects: string[] = []): Promise<any[]> {
+    let cache = CachedMap.get(src)
+    if (cache) {
+        return cache.promise.then(() => getExpectFromWindow(expects))
+    } else {
+        cache = addScriptTag(src)
+        CachedMap.set(src, cache)
+        return cache.promise.then(() => getExpectFromWindow(expects))
+    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useMemo, FunctionComponent } from 'react';
-import fetchJsFromCDN from './fetch-js-from-cdn';
-import './deps/aliplayercomponents-1.0.5.min.js';
-import './index.css';
+import React, { useEffect, useRef, useMemo, FunctionComponent } from 'react'
+import fetchJsFromCDN from './fetch-js-from-cdn'
+import './deps/aliplayercomponents-1.0.5.min.js'
+import './index.css'
 
-const SOURCE_URL = 'https://g.alicdn.com/de/prismplayer/2.8.2/aliplayer-min.js';
+const SOURCE_URL = 'https://g.alicdn.com/de/prismplayer/2.8.7/aliplayer-min.js'
 
 interface Props {
     config?: any;
@@ -12,30 +12,30 @@ interface Props {
 
 const Aliplayer: FunctionComponent<Props> & { components: any } = ({ config, onGetInstance }) => {
     if (!config) {
-        throw new Error('Missing Aliplayer config');
+        throw new Error('Missing Aliplayer config')
     }
 
-    const id = useMemo(() => `aliplayer-${Math.floor(Math.random() * 1000000)}`, []);
-    const player = useRef(null);
+    const id = useMemo(() => `aliplayer-${Math.floor(Math.random() * 1000000)}`, [])
+    const player = useRef(null)
 
     useEffect(() => {
         if (!id || player.current) { return }
-        fetchJsFromCDN(SOURCE_URL, ['Aliplayer'])
-        .then((data: any[]) => {
-            const Aliplayer = data[0];
-            if (player.current) { return }
-            player.current = new Aliplayer({
-                ...config,
-                "id": id,
-            }, (player: any) => {
-                onGetInstance && onGetInstance(player);
-            });
-        });
-    }, [id, config]);
 
-    return <div id={id}></div>
+        fetchJsFromCDN(SOURCE_URL, ['Aliplayer'])
+            .then(([Aliplayer]) => {
+                if (player.current) { return }
+                player.current = new Aliplayer({
+                    ...config,
+                    'id': id,
+                }, (player: any) => {
+                    onGetInstance && onGetInstance(player)
+                })
+            })
+    }, [id, config])
+
+    return (<div id={id}></div>)
 }
 
-Aliplayer.components = window.AliPlayerComponent;
+Aliplayer.components = window.AliPlayerComponent
 
 export default Aliplayer
